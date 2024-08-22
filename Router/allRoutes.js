eb4b74482a37a35e03fe3fcefffa02f04f2862ca
kevin43-sys <kevinvarraud@outlook.fr>
2024-08-22 15:15:53 +0200

    avancer

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,9 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    ];
+    new Route("/galerie", "La Galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
